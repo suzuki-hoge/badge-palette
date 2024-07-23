c4e339a544cdb0d3b8fa50ae54ab7c527e526808
suzuki-hoge <user.ryo@gmail.com>
2024-07-23 22:20:02 +0900

    install react-select and react-color.

--- src/content/BadgePalette.tsx
+++ src/content/BadgePalette.tsx
@@ -1,17 +1,72 @@
 import { useState } from 'react'
+import Select from 'react-select'
+import { TwitterPicker } from 'react-color'
+import CreatableSelect from 'react-select/creatable'
 
 const BadgePalette = () => {
-  const [label, setLabel] = useState('review')
-  const [message, setMessage] = useState('ask')
-  const [color, setColor] = useState('lightcyan')
-  const url = `https://img.shields.io/badge/${label}-${message}-${color}`
+  const [label, setLabel] = useState('github')
+  const [message, setMessage] = useState('')
+  const [color, setColor] = useState('white')
+  const url = `https://img.shields.io/badge/${label}-${message}-${color}?style=plastic&logo=${label}&logoColor=white`
+
+  const labels = [
+    { value: 'github', label: 'github' },
+    { value: 'springboot', label: 'springboot' },
+    { value: 'kotlin', label: 'kotlin' },
+  ]
+  const messages = [
+    { value: 'ask', label: 'ask', color: '8ED1FC' },
+    { value: 'must', label: 'must', color: 'EB144C' },
+  ]
 
   return (
     <div>
-      <input value={label} onChange={(e) => setLabel(e.target.value)} />
-      <input value={message} onChange={(e) => setMessage(e.target.value)} />
-      <input value={color} onChange={(e) => setColor(e.target.value)} />
+      <Select
+        defaultValue={{ value: 'github', label: 'github' }}
+        options={labels}
+        onChange={(label) => setLabel(label!.value)}
+      />
+      <CreatableSelect
+        options={messages}
+        onChange={(option) => {
+          setMessage(option!.value)
+          setColor(option!.color)
+        }}
+        styles={{
+          option: (base, { data }) => ({
+            ...base,
+            ...{
+              alignItems: 'center',
+              display: 'flex',
+              ':before': {
+                backgroundColor: `#${data.color}`,
+                borderRadius: 10,
+                content: '" "',
+                display: 'block',
+                marginRight: 8,
+                height: 10,
+                width: 10,
+              },
+            },
+          }),
+        }}
+        formatCreateLabel={(input) => input}
+      />
+      <TwitterPicker
+        color={color}
+        triangle={'hide'}
+        onChange={(color) => setColor(color.hex.replace('#', ''))}
+      />
       <img src={url} alt={'badge'} />
+      <p>{url}</p>
+      <button
+        onClick={(e) => {
+          console.log(url)
+          e.preventDefault()
+        }}
+      >
+        Insert
+      </button>
     </div>
   )
 }
--- src/content/index.tsx
+++ src/content/index.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import BadgePalette from './BadgePalette.tsx'
 
-const root = document.getElementById('partial-discussion-header')!
+const root = document.getElementById('partial-new-comment-form-actions')!
 const my = document.createElement('div')
 root.append(my)
 createRoot(my).render(<BadgePalette />)
